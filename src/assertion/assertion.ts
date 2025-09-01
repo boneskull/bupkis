5a02f19020dad83c61d7b207d8342bd5a8220cb3
Christopher Hiller <boneskull@boneskull.com>
2025-09-01 01:34:35 -0700

    chore: better assertion string representations

--- src/assertion/assertion.ts
+++ src/assertion/assertion.ts
@@ -56,30 +56,53 @@ export abstract class BupkisAssertion<
    * @returns String representation
    */
   public toString(): string {
-    const expand = (zodType: z.ZodType): string => {
-      switch (zodType.def.type) {
+    const expand = (zodType: z.core.$ZodType | z.ZodType): string => {
+      const def = 'def' in zodType ? zodType.def : zodType._zod.def;
+      switch (def.type) {
         case 'custom': {
           const meta = BupkisRegistry.get(zodType);
           if (meta?.name) {
             // our name
-            return `${meta.name}`;
+            return `{${meta.name}}`;
           } else if ('Class' in zodType._zod.bag) {
             // internal Zod class name. will probably break.
-            return (zodType._zod.bag.Class as new (...args: any[]) => any).name;
+            return `{${(zodType._zod.bag.Class as new (...args: any[]) => any).name}}`;
           }
-          return 'custom';
+          return '{custom}';
         }
+        case 'default':
+          return `{${expand((def as z.core.$ZodDefaultDef).innerType)}}`;
         case 'enum':
-          return `${(zodType as z.ZodEnum<any>).options.join(' / ')}`;
+          return `${Object.keys((def as z.core.$ZodEnumDef<any>).entries as Record<PropertyKey, unknown>).join(' / ')}`;
+        case 'intersection':
+          return `${expand((def as z.core.$ZodIntersectionDef<z.core.$ZodType>).left)} & ${expand((def as z.core.$ZodIntersectionDef<z.core.$ZodType>).right)}`;
         case 'literal':
-          return [...(zodType as z.ZodLiteral).def.values].join(' / ');
+          return (def as z.core.$ZodLiteralDef<any>).values
+            .map((value) => `"${value}"`)
+            .join(' / ');
+        case 'map':
+          return `{Map<${expand((def as z.core.$ZodMapDef).keyType)}, ${expand((def as z.core.$ZodMapDef).valueType)}>`;
+        case 'nonoptional':
+          return `${expand((def as z.core.$ZodNonOptionalDef).innerType)}!`;
+        case 'nullable':
+          return `${expand((def as z.core.$ZodNullableDef).innerType)}? | null`;
+        case 'optional':
+          return `${expand((def as z.core.$ZodOptionalDef).innerType)}?`;
+        case 'record':
+          return `{Record<${expand((def as z.core.$ZodRecordDef).keyType)}, ${expand((def as z.core.$ZodRecordDef).valueType)}>`;
+        case 'set':
+          return `{Set<${expand((def as z.core.$ZodSetDef).valueType)}>`;
+
+        case 'tuple':
+          return `[${(def as z.core.$ZodTupleDef).items.map(expand).join(', ')}]`;
         case 'union':
-          return ((zodType as z.ZodUnion<any>).options as z.ZodType[])
+          return (
+            (def as z.core.$ZodUnionDef<any>).options as z.core.$ZodType[]
+          )
             .map(expand)
             .join(' | ');
-        // falls through
         default:
-          return `{${zodType.def.type}}`;
+          return `{${def.type}}`;
       }
     };
     return `"${this.slots.map(expand).join(' ')}"`;
@@ -97,14 +120,13 @@ export abstract class BupkisAssertion<
     }
   }
 
-  protected parseSlotForLiteral<T extends (typeof this.slots)[number]>(
-    slot: T,
+  protected parseSlotForLiteral<Slot extends Slots[number]>(
+    slot: Slot,
     i: number,
     arg: unknown,
   ): boolean | ParsedResult<Parts> {
     const meta = BupkisRegistry.get(slot) ?? {};
     // our branded literal slots are also tagged in meta for runtime
-    // const metadata = BupkisRegistrySchema.safeParse(meta);
     if (kStringLiteral in meta) {
       if ('value' in meta) {
         if (arg !== meta.value) {
@@ -124,20 +146,29 @@ export abstract class BupkisAssertion<
           };
         }
       } else {
-        debug('Invalid metadata for slot', i, 'with value', arg);
-        return {
-          assertion: `${this}`,
-          reason: `Invalid metadata for slot ${i}`,
-          success: false,
-        };
+        /* c8 ignore next */
+        throw new TypeError(
+          `Invalid metadata for slot ${i} with value ${inspect(arg)}`,
+        );
       }
       return true;
-      // skip from impl params
     }
     return false;
   }
 
-  // TODO: support stackStartFn
+  /**
+   * Translates a {@link z.ZodError} into an {@link AssertionError} with a
+   * human-friendly message.
+   *
+   * @remarks
+   * This does not handle parameterized assertions with more than one parameter
+   * too cleanly; it's unclear how a test runner would display the expected
+   * values. This will probably need a fix in the future.
+   * @param stackStartFn The function to start the stack trace from
+   * @param zodError The original `ZodError`
+   * @param values Values which caused the error
+   * @returns New `AssertionError`
+   */
   protected translateZodError(
     stackStartFn: (...args: any[]) => any,
     zodError: z.ZodError,
@@ -149,10 +180,12 @@ export abstract class BupkisAssertion<
     for (const [keypath, errors] of Object.entries(flat.fieldErrors)) {
       pretty += `; ${keypath}: ${(errors as unknown[]).join('; ')}`;
     }
-    const [actual, expected] = values as unknown as [unknown, unknown];
+
+    const [actual, ...expected] = values as unknown as [unknown, ...unknown[]];
+
     return new AssertionError({
       actual,
-      expected,
+      expected: expected.length === 1 ? expected[0] : expected,
       message: `Assertion ${this} failed: ${pretty}`,
       operator: `${this}`,
       stackStartFn,
---
+++ test/strings.test.ts
@@ -0,0 +1,22 @@
+import Debug from 'debug';
+import { describe, it } from 'node:test';
+
+import { AsyncAssertions, SyncAssertions } from '../src/assertion/index.js';
+import { expect } from '../src/expect.js';
+
+const debug = Debug('bupkis:test');
+
+describe('Assertion string representations', () => {
+  it('should contain no duplicates', () => {
+    const actual: Set<string> = new Set();
+    const expected: string[] = [];
+    for (const assertion of [...SyncAssertions, ...AsyncAssertions]) {
+      const str = assertion.toString();
+      debug(str);
+      actual.add(str);
+      expected.push(str);
+    }
+
+    expect(actual.size, 'to be', expected.length);
+  });
+});
